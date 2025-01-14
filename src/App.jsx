9ea839caaf25f064df7c20fbde29976cc20b808d
sinkingcircle <vicky@botcode.com>
2025-01-14 17:50:40 +0530

    new changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,59 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { useEffect } from "react";
+import "./App.css";
+import search from "./search.svg";
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+//API KEY: b9aef7d
+
+const API_URL = "http://www.omdbapi.com?apikey=b9aef7d";
+
+const App = () => {
+
+  const searchMovies = async (title) => {
+
+      const response = await fetch(`${API_URL}&s=${title}`);
+      const data = await response.json();
+
+      console.log(data.Search);
+  }
+
+
+
+
+useEffect(() => {
+  searchMovies("Spiderman");
+
+
+} , []
+);
+
+
+  return(
+    <div className="app">
+
+      <h1>MovieLand</h1>
+
+      <div className="search">
+
+        <input
+          placeholder="Search for a movie"
+          value="Superman"
+          onChange={() => {}}
+
+
+          />
+
+          <img  
+            src={search}
+            alt = "search"
+            />
+
+
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
+    </div>
+  ) 
 }
 
-export default App
+export default App;
